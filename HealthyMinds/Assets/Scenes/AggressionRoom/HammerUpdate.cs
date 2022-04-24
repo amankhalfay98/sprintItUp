6cc9e5925cbaf6d078860464801f6f560e0f64a2
Sripradha17 <sbhat@stevens.edu>
2022-04-24 12:51:03 -0400

    Temp commit

--- HealthyMinds/Assets/Scenes/AggressionRoom/HammerUpdate.cs
+++ HealthyMinds/Assets/Scenes/AggressionRoom/HammerUpdate.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class HammerUpdate : MonoBehaviour
 {
     public Vector3 anglesToRotate;
+    public CharacterController controller;
+
+        public float speed = 5f;
+        public float gravity = -15f;
+
+        Vector3 velocity;
+
+        bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +23,10 @@ public class HammerUpdate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-   	RaycastHit hit;
-   	// Casts the ray and get the first game object hit
-   	Physics.Raycast(ray, out hit);
-   	Debug.Log("This hit at " + hit.point );
-            // Debug.Log(anglesToRotate);
-            // this.transform.Rotate(anglesToRotate * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log(anglesToRotate);
+            this.transform.Rotate(anglesToRotate * Time.deltaTime);
         }
     }
 }
